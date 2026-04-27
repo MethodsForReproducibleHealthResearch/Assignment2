a5c1efa99e4b4ee0225fea6c9ce5baecc9fbe500
Helene <hnepomuc@stanford.edu>
2026-04-27 11:46:21 -0700

    Update analysis.r

--- analysis.r
+++ analysis.r
@@ -1,1 +1,8 @@
 
+> summary(cohort)
+
+> hist(cohort$age, main = "Age Distribution", xlab = "Age")
+
+> model <- lm(cohort$cost ~ cohort$smoke + cohort$age + cohort$female + cohort$cardiac)
+> 
+> summary(model)
